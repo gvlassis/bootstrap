68586addd386c287b8a33f3551b8ddff1f3e1ccf
Georgios Vlassis <gvlassis@mailbox.org>
2026-01-30 13:20:40 +0100

    Atuin rework

--- src/connect_atuin.sh
+++ src/connect_atuin.sh
@@ -2,11 +2,11 @@ function connect_atuin(){
     local atuin_username="${1}"
     local atuin_password="${2}"
 
-    PATH="${HOME}/.local/bin:${PATH}"
-
-    atuin login -u "${atuin_username}" -k "" -p "${atuin_password}"
-
-    # Fixes: attempting to decrypt with incorrect key
-    atuin store purge
-    atuin sync
+    # PATH="${HOME}/.local/bin:${PATH}"
+    #
+    # atuin login -u "${atuin_username}" -k "" -p "${atuin_password}"
+    #
+    # # Fixes: attempting to decrypt with incorrect key
+    # atuin store purge
+    # atuin sync
 }
